77cad98aa4d742eb2639716f2974c56c015022a7
Allen <allengabrielle.pasion@tup.edu.ph>
2025-11-18 21:02:34 +0800

    seventh Commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById('contactButton').addEventListener('click', function() {
     // This makes a simple alert box pop up when the button is clicked
-    alert("Thanks for visiting! Please email me at your.email@example.com.");
+    alert("Thanks for visiting! Please email me at allenpasion05@gmail.com.");
 });
